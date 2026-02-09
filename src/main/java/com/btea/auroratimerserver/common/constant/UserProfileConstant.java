25ec9408dc5147b609ed6b736463fe580e8d88cd
TwentyfiveBTea <1248102041@qq.com>
2026-02-10 02:11:52 +0800

    feat: 添加用户常量类
    - UserProfileConstant.java: 用户相关常量（默认职位、重置密码等）

---
+++ src/main/java/com/btea/auroratimerserver/common/constant/UserProfileConstant.java
@@ -0,0 +1,19 @@
+package com.btea.auroratimerserver.common.constant;
+
+/**
+ * @Author: TwentyFiveBTea
+ * @Date: 2026/2/9 18:20
+ * @Description: 用户信息常量
+ */
+public class UserProfileConstant {
+
+    /**
+     * 默认职位
+     */
+    public static final String DEFAULT_POSITION = "未设置";
+
+    /**
+     * 默认密码
+     */
+    public static final String RESET_PASSWORD = "Aurora666";
+}
